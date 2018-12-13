a2f796f207efff028009c097070853d5f19fc27e
Risfio <risfio1982@gmail.com>
2018-12-13 23:27:33 +0300

    forts: little fixes in options.fromstring function.

--- options/__init__.py
+++ options/__init__.py
@@ -5,32 +5,36 @@ Initial options package
 import re
 
 
-def fromstring(str):
+def fromstring(input_string):
 
     temp = ""
     result = []
 
     ba_reg = re.compile(r'^\w{2,2}')
-    result.append(ba_reg.findall(str)[0])
+    result.append(ba_reg.findall(input_string)[0])
     temp = ba_reg.subn("", str, 1)[0]
 
 
     # Get strike price and deals type(marginal or premial)
+
     strike_reg = re.compile(r'^\d{2,}')
     result.append(strike_reg.findall(temp)[0])
     temp = strike_reg.subn("", temp, 1)[0]
 
     #Get type of settlement
+
     settlement_reg = re.compile(r'^[A|B]{1,1}')
     result.append(settlement_reg.findall(temp)[0])
     temp = settlement_reg.subn('', temp, 1)[0]
 
     #Get month
-    month_reg = re.compile(r'^[L]{1,1}')
+
+    month_reg = re.compile(r'^[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X]{1,1}')
     result.append(month_reg.findall(temp)[0])
     temp = month_reg.subn('', temp, 1)[0]
 
     #Get year
+
     result.append(temp)
 
     return result
