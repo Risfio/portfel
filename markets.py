f04e435abed4404d627c0f9065cf18aaeb79cc6f
Risfio <risfio1982@gmail.com>
2019-01-14 21:47:14 +0300

    forts: Добавил описание для рынков ФОРТС.

--- markets.py
+++ markets.py
@@ -9,7 +9,7 @@ class MarketBase:
     """
     Это метакласс, а может и нет !!??
     Использовать примерно так :
-    >>> class Market(MarketFORTSBase):
+    >>> class Market(MarketFORTS):
     >>>     deals = [
     >>>     {unit:[option class exemplary]; money:1000; deal_type:"SELL"; dt:"12.12.2018:19:45"; et=False},
     >>>     [[option class exemplary], 1000, "SELL", "12.12.2018:19:45", False],
@@ -17,16 +17,76 @@ class MarketBase:
     >>>     ]
     >>>
     >>> market = Market()
+
+    @:param name: str название для портфеля на случай , если будем в БД сохранять
     """
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+
+    def calculate_deals(self):
+        """
+        Собственно просчитываем все сделки в поле deals.
+        Этот метод прописывается для каждого вида рынка отдельно. Для
+        ФОРТСА свой, для ММВБ свой
+        :return: None (что возвращать пока не знаю)
+        """
         pass
 
 
-class LimitationsOnClientAccounts:
+class MarketFORTS(MarketBase):
+    def calculate_deals(self):
+        """
+        Вызов метода для вычисления значений для рынка ФОРТС.
+        :return:
+        """
+        return None
+
+
+class MarketTradeReglaments:
+    """
+    Устанавливает порядок вычисления LimitationsOnClientAccounts и PositionsOnClientsAccounts по таймфрейму.
+    """
     pass
 
 
-class PositionsOnClientAccounts:
+class LimitationsOnClientAccountsBase:
+    """
+    Класс отражающий таблицу QUIK - ограничения по клиентским счетам/
+    Отражает состояние счетов на текущую сессию между клирингами.
+
+    :param LimitOfOpenPositions : 	Лимит открытых позиций - денежные ср-ва трейдера на
+                                    срочном рынке (текущие лимиты открытых  позиций в денежном выражении);
+    :param CurrentNetPosition : 	Текущая чистая позиция - денежные ср-ва трейдера , зарезервированные под
+                                    гарантийное обеспечение по всем позициям трейдера на срочном рынке и на
+                                    отрицательную вариационную маржу(при ее наличии);
+    :param VariationMargin : 	Вариационная маржа - показывает значение вариационной маржи по всем позициям
+                                трейдера по текущей торговой сессии;
+    :param AccruedIncome : 	Накопленный доход - показывает значение денежных ср-в, которые трейдер заработал
+                            донаступления клиринга;
+    :param ExchangeFee : Биржевые сборы - отображает биржевые комиссионные сборы по всем сделкам клиента за
+                        текущую торговую сессию.
+    :return
+    """
+    pass
+
+
+class PositionsOnClientAccountsBase:
+    """
+    Класс отражающий таблицу QUIK - позиции по клиентским счетам.
+    Отражает состояние  клиентскиъ счетов после клиринга и соответственно экспирации.
+
+    :param  ShortName: Краткое название - краткий код торгового инструмента;
+    :param  IncomingLongPosition: Входящая длинная позиция - кол-во контрактов в длинных позициях по
+                                    торговому инструменту срочного рынка на начало торгов;
+    :param  IncomingShortPosition: Входящая короткая позиция - кол-во контрактов в коротких позициях
+                                    по торговому инструменту срочного рынка на начало торгов
+    :param  CurrentLongPosition: Текущая длинная позиция - количество текущих длинных позиций по торговому инструменту;
+    :param  CurrentShortPosition: Текущая короткая позиция - количество текущих коротких позиций по
+                                торговому инструменту;
+    :param  VariationMargin: Вариационная маржа - текущая вариационная маржа по каждому инструменту;
+
+    :return
+    """
     pass
 
 
@@ -40,23 +100,23 @@ class BrockerDeal:
     сделки. Класс сдесь нужен, только, для того, чтоб контролировать процесс создания и
     небольшого удобства в визуализации ( типа указывать BUY/SELL при использовании.
 
-    :param unit : exemplary of classes like Option, Future
+    :param dealunit : exemplary of classes like Option, Future
     :param money : allways POSITIVE number - sum of transaction
-    :param type : int number 0 or 1 (sell or buy) or for visualize use "BUY" and "SELL"
+    :param dealtype : int number 0 or 1 (sell or buy) or for visualize use "BUY" and "SELL"
     :param dt : date and time of transaction execution
-    :param et : True or False - set if transaction was executed or not
+    :param et : True or False - set if transaction was executed or not (et - transaction execution)
     """
     def __init__(self, market_unit, money, deal_type=1, dt=datetime.now(), et=False):
-        self._unit = market_unit
-        self._money = money
+        self.dealunit = market_unit
+        self.money = money
 
         if deal_type == "BUY":
-            self._type = 1
+            self.dealtype = 1
         elif deal_type == "SELL":
-            self._type = 0
+            self.dealtype = 0
         else:
-            self._type = deal_type
+            self.dealtype = deal_type
 
-        self._dt = dt
-        self._et = et
+        self.dt = dt
+        self.et = et
 
--- tests/temp.py
+++ tests/temp.py
@@ -3,15 +3,13 @@ import re
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-
+from markets import MarketFORTS
 from options import _fromstring, Option
 
 
 if __name__ == "__main__":
-    s = 'Si66500BC0'
-    params = _fromstring(s)
-    opt = Option(*params)
-    print(opt)
-
-
+    # String for test options
+    # s = 'Si66500BC0'
+    class Market1(MarketFORTS):
+        pass
 
