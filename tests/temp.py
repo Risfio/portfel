0190c61a27e4ed2a71bbc0363c9e78a1d5c131ae
Risfio <risfio1982@gmail.com>
2019-01-15 20:26:41 +0300

    forts: remove options package. Transform it to simple module

--- tests/temp.py
+++ tests/temp.py
@@ -4,12 +4,12 @@ import re
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from markets import MarketFORTS
-from options import _fromstring, Option
+from options import fromstring, Option
 
 
 if __name__ == "__main__":
     # String for test options
-    # s = 'Si66500BC0'
-    class Market1(MarketFORTS):
-        pass
+    s = 'Si66500BC0'
+    opt = fromstring(s)
+    print(opt)
 
