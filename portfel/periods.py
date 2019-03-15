f51bc5d04138c1be5d4e9a5b41d24283e4bf1fe7
Risfio <risfio1982@gmail.com>
2019-03-15 21:10:36 +0300

    periods: TODO: test results for _get_periods function

--- portfel/periods.py
+++ portfel/periods.py
@@ -10,9 +10,9 @@ TODO:
 import numpy as np
 
 
-PERIOD_TREND_LOWERING = lambda x: x > 0 and True or False
-PERIOD_TREND_GROWING = lambda x: x < 0 and True or False
-PERIOD_TREND_CONTINUOS = lambda x: x == 0 and True or False
+PERIOD_TREND_LOWERING = lambda x: x > 1 and True or False
+PERIOD_TREND_GROWING = lambda x: x < 1 and True or False
+PERIOD_TREND_CONTINUOS = lambda x: x == 1 and True or False
 
 
 class PeriodsContainer:
@@ -20,11 +20,39 @@ class PeriodsContainer:
     def __init__(self, values):
         self.values = np.array(values)
 
+    def _get_trend(self, arg):
+        result = False
+        for func in [PERIOD_TREND_CONTINUOS,
+                     PERIOD_TREND_GROWING,
+                     PERIOD_TREND_LOWERING]:
+            if func(arg) == True:
+                result = func
+        return result
+
     def _get_periods(self):
-        # Все значения сдвигаем в положительную зону на асолютную величину
-        # минимального значения. Например [-1, 0, 1] будет [0,1,2]
-        _shift_value = np.abs(self.values.min())
-        _values = (for x in np.nditer(self.values, op_flags=["readwrite"]) and x += _shift_value)
+        n = 0
+        periods = []
+        while n < (self.values.size -1):
+            period = [n, ]
+            trend = True
+            trend_func = None
+            try:
+                trend_func = self._get_trend(self.values[n]/self.values[n+1])
+            except Exception as e:
+                trend_func = self._get_trend(self.values[n])
+            while n < (self.values.size -1) and trend == True:
+                try:
+                    n += 1
+                    trend = trend_func(self.values[n]/self.values[n+1])
+                except IndexError as e:
+                    pass
+                except Exception as e:
+                    trend = trend_func(self.value[n])
+            period.append(n)
+            periods.append(period)
+        # debug
+        print(periods)
+        return periods
 
 
 class Period(tuple):
