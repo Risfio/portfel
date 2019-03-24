96eca12acbe6af0d169d0c85160283130c03e39f
Risfio <risfio1982@gmail.com>
2019-03-24 10:59:05 +0300

    periods: finally get norm period function.

--- portfel/periods.py
+++ portfel/periods.py
@@ -8,11 +8,12 @@ TODO:
 """
 
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
-PERIOD_TREND_LOWERING = lambda x: x > 1 and True or False
-PERIOD_TREND_GROWING = lambda x: x < 1 and True or False
-PERIOD_TREND_CONTINUOS = lambda x: x == 1 and True or False
+PERIOD_TREND_LOWERING = lambda x, y: x > y and True or False
+PERIOD_TREND_GROWING = lambda x, y: x < y and True or False
+PERIOD_TREND_CONTINUOS = lambda x, y: x == y and True or False
 
 
 class PeriodsContainer:
@@ -20,36 +21,32 @@ class PeriodsContainer:
     def __init__(self, values):
         self.values = np.array(values)
 
-    def _get_trend(self, arg):
-        result = False
-        for func in [PERIOD_TREND_CONTINUOS,
-                     PERIOD_TREND_GROWING,
-                     PERIOD_TREND_LOWERING]:
-            if func(arg) == True:
+    def _get_trend(self, arg1, arg2):
+        result = None
+        for func in [PERIOD_TREND_LOWERING,
+                PERIOD_TREND_GROWING,
+                PERIOD_TREND_CONTINUOS]:
+            if func(arg1, arg2) == True:
                 result = func
+                break
         return result
 
     def _get_periods(self):
-        n = 0
         periods = []
-        while n < (self.values.size -1):
-            period = [n, ]
-            trend = True
-            trend_func = None
-            try:
-                trend_func = self._get_trend(self.values[n]/self.values[n+1])
-            except Exception as e:
-                trend_func = self._get_trend(self.values[n])
-            while n < (self.values.size -1) and trend == True:
-                try:
-                    n += 1
-                    trend = trend_func(self.values[n]/self.values[n+1])
-                except IndexError as e:
-                    pass
-                except Exception as e:
-                    trend = trend_func(self.value[n])
-            period.append(n)
-            periods.append(period)
+        period = []
+        start_trend = self._get_trend(self.values[0], self.values[1])
+        previos_value = 0
+        period.append(previos_value)
+        for x in range(self.values.size)[1:]:
+            if start_trend(self.values[previos_value], self.values[x]) == False:
+                period.append(x-1)
+                periods.append(period)
+                period = [x, ]
+                start_trend = self._get_trend(self.values[previos_value], self.values[x])
+            elif x == (self.values.size - 1):
+                period.append(x)
+                periods.append(period)
+            previos_value = x
         return periods
 
 
