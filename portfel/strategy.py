6452d751a7de72fa7d5555d8c4bff5fb8dea895c
Risfio <risfio1982@gmail.com>
2019-03-28 19:48:23 +0300

    strategy: developing strategy initialization

--- portfel/strategy.py
+++ portfel/strategy.py
@@ -6,6 +6,19 @@ Strategy classes
 class StrategyBase(type):
     def __new__(cls, name, bases, attrs):
         _deals = {}
+
+        new_attrs = {}
+
+        if "range" not in attrs.keys():
+            raise Exception("[range] attribute must be setted")
+        else:
+            new_attrs.update({'range': attrs.pop("range")})
+
+        if "step" not in attrs.keys():
+            raise Exception("[step] value must be setted in Strategy declaration")
+        else:
+            new_attrs.update({'step': attrs.pop("step")})
+
         for key, value in attrs.items():
             if not key.startswith('__'):
                 _deals.update({key: value})
