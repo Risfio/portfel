8957d7a7ad4bcec4ffc1d0e2665a0a89ad2dd3e3
Risfio <risfio1982@gmail.com>
2019-04-11 20:22:26 +0300

    analize: start develop main class Analize

--- portfel/analize/utils.py
+++ portfel/analize/utils.py
@@ -1,3 +1,17 @@
 """
 ... somethig
+
+Analize class have same usage:
+
+>>> class analize(Analize):
+>>>     def less_then(self, arg=0, msg="Revenues range less 0"):
+>>>         result = super(analize, self).less_then(arg, msg)
+>>>         return pd.DataFrame(data=result, index=msg)
+
+
 """
+
+
+class Analize:
+    pass
+
--- tests/test_analize.py
+++ tests/test_analize.py
@@ -1,20 +1,12 @@
 import unittest
+from portfel.analize import Analize
 
 
 class TestAnalize(unittest.TestCase):
     def test_add_filter_method(self):
         print("*" * 10, "Start analize test", "*" * 10)
-        class Analize:
-            def __init__(self, **kwargs):
-                for k, v in kwargs.items():
-                    self.__setattr__(k, v)
-
-            def less_then(self, arg):
-                return "im less then"
-
-        analize = Analize()
-
-        self.assertTrue(hasattribute(analize, "less_then")
+        class analize(Analize):
+            pass
 
 if __name__ == "__main__":
     unittest.main()
