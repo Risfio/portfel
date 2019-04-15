20e3dae656827bfae6d9d82cce83541f26abca7c
Risfio <risfio1982@gmail.com>
2019-04-15 20:28:23 +0300

    analize: in develop strategy analize

--- portfel/analize/utils.py
+++ portfel/analize/utils.py
@@ -4,7 +4,7 @@
 Analize class have same usage:
 
 >>> class analize(Analize):
->>>     def less_then(self, arg=0, msg="Revenues range less 0"):
+>>>     def less_then(self, arg=0, lbl="less then %VAL%" msg="Revenues range less 0"):
 >>>         result = super(analize, self).less_then(arg, msg)
 >>>         return pd.DataFrame(data=result, index=msg)
 
@@ -16,6 +16,22 @@ import pandas as pd
 
 
 class Analize:
-    def less_then(self, arg, msg):
-        return pd.DataFrame(data=[arg], index=[msg])
+
+    def __init__(self, items=[]):
+        self._items = items
+
+    def get(self, **kwargs):
+        analize_methods = []
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                analize_methods.append((k, v))
+
+        result = pd.DataFrame()
+        for method in analize_methods:
+            name, arg = method
+            result.append(self.__dict__(name)(arg))
+        return result
+
+    def less_then(self, arg, lbl="less then %VAL%", msg="Here description"):
+        return pd.DataFrame(data=[[arg, lbl, msg]], columns=["value", "name", "description"])
 
--- tests/test_analize.py
+++ tests/test_analize.py
@@ -1,16 +1,37 @@
 import unittest
 from portfel.analize import Analize
+from portfel.markets import BrockerDeal
+from portfel.options import fromstring
 
+from portfel.strategy import Strategy
 
 class TestAnalize(unittest.TestCase):
+
     def test_add_filter_method(self):
-        print("*" * 10, "Start analize test", "*" * 10)
+        # Переопределение матода less_then и есть то, что анализирует данная
+        # функция
         class analize(Analize):
-            def less_then(self, arg, msg):
-                return super(analize, self).less_then(arg, msg)
+            def less_then(self, arg, lbl="less then %VAL%", msg="Here description"):
+                return super(analize, self).less_then(arg, lbl, msg)
 
         an = analize()
-        print("*" * 10, an.less_then(0, "on duty"), "*" * 10)
+        print("*" * 10, "\r\n", an.less_then(0), "\r\n", "*" * 10)
+
+    def test_strategy_analize(self):
+        class strategy(Strategy):
+            range = (62000, 72000)
+            step = 250
+            buy_call = BrockerDeal(fromstring(self.str_CALL), 1000)
+
+        class analize(Analize):
+            def less_then(self, arg, lbl="less then %VAL%", msg="Here description"):
+                revenues = self._items.values.all()
+                num_losses = len(for x in revenues['revenues'] if x < arg)
+                result = num_losses/revenues.size
+                return result
+
+        an = analize(strategy)
+        print("Result of strategy analize:\r\n", an.get(less_then = 0))
 
 
 if __name__ == "__main__":
