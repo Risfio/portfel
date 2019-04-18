51efecb40bd4f62037d7a1f7216c5efdd8fbfc00
Risfio <risfio1982@gmail.com>
2019-04-18 20:45:12 +0300

    filters:initial commit

--- portfel/market/filters.py
+++ portfel/market/filters.py
@@ -1,2 +1,20 @@
+
+
+
 class Filter:
-    pass
+    def __init__(self, data, **kwargs):
+        # check filters param
+        self._data = data
+        self.result = None
+        for name, arg in kwargs.items():
+            if name in self.__dict__.keys():
+                result = self.__dict__[name](arg)
+
+    def less_then(self, arg):
+        result = pd.DataFrame(data={"revenues":[], "base active":[]})
+        for x in range(0, self._data.size):
+            rev, ba = self._data[x]
+            if rev < 0:
+                result.update([rev, data])
+        return result
+
