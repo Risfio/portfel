3d6143d59f157345fb4092a93db5273e33cf1755
Risfio <risfio1982@gmail.com>
2019-04-21 14:11:46 +0300

    filter:and main functional of filter. Need link it to DataSet class.

--- portfel/market/filters.py
+++ portfel/market/filters.py
@@ -5,7 +5,7 @@ class Filter:
     def __init__(self, data, **kwargs):
         self._data = data
 
-    def _sort_data(self, ranges):
+    def _sort_data(self, indexes):
         """
         :ranges array: массив значений которые надо выбрать.
                         Например, после фильтрации получаем значение ranges
@@ -15,9 +15,31 @@ class Filter:
         result = []
         n = 0
         for val in self._data.iterrows():
-            if n in ranges:
+            if n in indexes:
                 result.append(self._data.values[n])
             n += 1
         return pd.DataFrame(data=result, columns=self._data.columns)
 
+    def less_then(self, arg, column):
+        """
+        Filter dataframe for val's less then arg in specified DataFrame
+        column.
+        :param arg: int value
+        :param column: specified column in DataFrame object
+        :return: array with indecies what matches the condition
+        """
+        n = 0
+        indexes = []
+        for val in self._data[column]:
+            if val < arg:
+                indexes.append(n)
+            n += 1
+        return indexes
 
+    def __call__(self, **kwargs):
+        result = None
+        for name, arg in kwargs.items():
+            if hasattr(self, name) is True:
+                result = self.__getattribute__(name)(arg, column=kwargs['column'])
+                break
+        return self._sort_data(result)
