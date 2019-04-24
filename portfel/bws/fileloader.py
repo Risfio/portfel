e4de320ac2ed421efc84284357b64911e1ea4fe4
risfio <risfio1982@gmail.com>
2019-04-24 20:52:26 +0300

    fileloader: FileLoader initialization and tests

--- portfel/bws/fileloader.py
+++ portfel/bws/fileloader.py
@@ -8,11 +8,46 @@ import os
 
 
 class FileLoader:
+
     def __init__(self, path=""):
-        if os.path.exists(path):
-            self.base_dir = path
+        self.base_dirs = []
+        try:
+            pathes_iterator = iter(path)
+        except TypeError as te:
+            self.base_dirs.append([self._get_dir(path)])
         else:
-            raise Exception("Directory %s not found".format(path))
+            for dr in path:
+                self.base_dirs.append(self._get_dir(path))
         return super(FileLoader, self).__init__()
 
+    def _get_dir(self, path):
+        if not os.path.isdir(path) is True:
+            raise Exception("Directory {0} not found!".format(path))
+        return path
 
+    def _get_file(self, file_name):
+        temp_pathes = []
+        for dr in self.base_dirs:
+            path = self._construct_file_path(self._get_dir(dr), file_name)
+            if os.path.isfile(path):
+                temp_pathes.append(path)
+        # check for multiples instances with same name in different folders
+        if len(temp_pathes) > 0:
+            raise Exception("File with name {0} in multiple directories".format(file_name))
+        return temp_pathes[0]
+
+    def _construct_file_path(self, file_name, directory, ext=".csv"):
+        """
+        Can be rewritable for specified FileLoader instance.
+
+        Use like this:
+        >>> class FLoader(FileLoader):
+        >>>     def _construct_file_path(self, file_name, directory="", ext=".csv"):
+        >>>         result = "".join(["somefile_", file_name])
+        >>>         return super(Floader, self)._construct_file_path(self, result, ext)
+
+        :param prefix:
+        :param ext:
+        :return:
+        """
+        return os.path.join(directory, "".join([file_name, ext]))
--- tests/test_main_bws.py
+++ tests/test_main_bws.py
@@ -4,9 +4,15 @@ import os
 from portfel.bws import BWS
 from portfel.bws import FileLoader
 
+
+def print_info(msg):
+    print("*" *10, "\r\n", msg, "\r\n", "*" *10)
+
+
 class TestBWS(unittest.TestCase):
     def test_fileloader(self):
-        floader = FileLoader('/c/SHIT')
+        floader = FileLoader(path=os.path.join(os.path.expanduser('~'), 'documents', 'projects', 'BWS'))
+        self.assertTrue(os.path.isdir(floader.base_dirs[0]))
 
 
 if __name__ == '__main__':
