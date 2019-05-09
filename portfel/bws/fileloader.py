7b7b20c32236de6ebf9737fb529a3dcbec10b7a4
Risfio <risfio1982@gmail.com>
2019-05-09 16:12:28 +0300

    fileloader:finish fileloader for QUIK files. Add instruction for manual files creation

--- portfel/bws/fileloader.py
+++ portfel/bws/fileloader.py
@@ -9,6 +9,24 @@ import os
 
 class FileLoaderBase:
 
+    def __init__(self, directory="", names=(), file_name=""):
+        self._directory = directory
+        self._file_name = file_name
+
+        # check columns names
+        if not type(names) is tuple:
+            raise Exception("names parameter must be tuple")
+        else:
+            self._names = names
+
+    @property
+    def directory(self):
+        return self._directory
+
+    @directory.setter
+    def directory(self, arg):
+        self._directory = arg
+
     def _get_file_path(self, directory, file_name, ext=".txt"):
         file_path = os.path.join(directory, "".join([file_name, ext]))
         if os.path.isdir(directory):
@@ -20,20 +38,40 @@ class FileLoaderBase:
             raise Exception("Directory {0} not found!".format(directory))
         return file_path
 
-    def _load_pandas_data(self, path="", directory="", file_name="", sep=";", encoding="utf-8", ext=".txt"):
-        return pd.read_csv(path, encoding=encoding, sep=sep)
+    def _load_pandas_data(self, path="", directory="", file_name="", sep=";", encoding="utf-8", ext=".txt", names=[]):
+        if len(names)==0:
+            names = self._names
+        elif len(names) > 0:
+            names = tuple(names)
+        return pd.read_csv(path, encoding=encoding, sep=sep, header=None, names=names)
 
     def _construct_file_name(self, file_name):
+        if file_name == "":
+            file_name = self._file_name
         return file_name
 
-    def read_data(self, file_name):
+    def read_data(self, file_name=""):
         return self._load_pandas_data(file_name=self._construct_file_name(file_name))
 
 
 class FileLoaderQUIK(FileLoaderBase):
-    def _load_pandas_data(self, path="", directory="", file_name="", sep="\t", encoding="utf-8", ext=".txt"):
-        directory = self.directory
+    """
+    Для загрузки CSV из файла созданного копипастой из QUIK.
+    Для создания файла следующие шаги:
+    1) В таблице QUIK "Текущие торги" должны быть поля Дата торгов, Код, ISIN, Мин, Макс, Откр., Закрытие, Объем
+    2) ПКМ - Компировать все
+    3) Сохраняем в текстовый файл emitents_ddmmyyyy.txt
+    4) Удаляем верхний ряд с названием колонок.
+    """
+
+    def __init__(self, directory="", names=(), file_name=""):
+        names = tuple(['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume'])
+        return super(FileLoaderQUIK, self).__init__(directory=directory, names=names, file_name=file_name)
+
+    def _load_pandas_data(self, path="", directory="", file_name="", sep="\t", encoding="utf-8", ext=".txt", names=[]):
+        if directory == "":
+            directory = self._directory
         path = self._get_file_path(directory, file_name)
         encoding = 'cp1251'
-        return super(FileLoaderQUIK, self)._load_pandas_data(path=path, encoding=encoding, sep=sep)
+        return super(FileLoaderQUIK, self)._load_pandas_data(path=path, encoding=encoding, sep=sep, names=names)
 
--- tests/test_main_bws.py
+++ tests/test_main_bws.py
@@ -19,18 +19,25 @@ class TestBWS(unittest.TestCase):
 
     def test_read_data(self):
         directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-
         quik = FileLoaderQUIK()
         quik.directory = directory
         data = quik.read_data("emitents")
-        #print_info(data['Наименование'])
 
     def test_fileload(self):
-        columns = ['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume']
+        columns = tuple(['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume'])
         directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
         file_name = "emitents_08052019.txt"
-        #data = pd.read_csv(os.path.join(directory, file_name), sep="\t", encoding='cp1251', header=None, names=columns)
-        #print(data)
+        quik = FileLoaderQUIK(directory=directory, file_name="emitents_08052019", names=columns)
+        data = quik.read_data()
+
+    def test_wrong_num_columns(self):
+        columns = tuple(['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume'])
+        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
+        file_name = "emitents_08052019.txt"
+        quik = FileLoaderQUIK(directory=directory, file_name="emitents_08052019")
+        data = quik.read_data()
+        # Test columns names espetional for QUIK file loader
+        self.assertEqual(columns, tuple(data.columns.tolist()))
 
 
 if __name__ == '__main__':
