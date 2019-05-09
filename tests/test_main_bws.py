dfa0a90d80c4cc8e313165d94f02c93c11df2327
Risfio <risfio1982@gmail.com>
2019-05-09 15:12:49 +0300

    fileloader:little test finished

--- tests/test_main_bws.py
+++ tests/test_main_bws.py
@@ -23,7 +23,14 @@ class TestBWS(unittest.TestCase):
         quik = FileLoaderQUIK()
         quik.directory = directory
         data = quik.read_data("emitents")
-        print_info(data['Наименование'])
+        #print_info(data['Наименование'])
+
+    def test_fileload(self):
+        columns = ['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume']
+        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
+        file_name = "emitents_08052019.txt"
+        #data = pd.read_csv(os.path.join(directory, file_name), sep="\t", encoding='cp1251', header=None, names=columns)
+        #print(data)
 
 
 if __name__ == '__main__':
