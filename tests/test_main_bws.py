1fb0ad78fdee4f1161237fe3a840ee04e6af6357
Risfio <risfio1982@gmail.com>
2019-05-30 20:26:29 +0300

    bws: clean project files before start develop BWS-MAIN

--- tests/test_main_bws.py
+++ tests/test_main_bws.py
@@ -1,71 +1,23 @@
 import unittest
-import os
+import os, sys
 import pandas as pd
 
-from portfel.bws import FileLoaderQUIK
+sys.path.append(os.getcwd())
+import portfelcfg
 
+TEST_DIR = os.path.join(portfelcfg.BASE_DIR, "tests", "TEMP")
+os.path.isdir(TEST_DIR)
 
 def print_info(msg):
     print("*" *10, "\r\n", msg, "\r\n", "*" *10)
 
 
 class TestBWS(unittest.TestCase):
-    def test_complex(self):
-        """
-        Complex testing. Removed msut.
-        :return:
-        """
-        dir_path = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-
-
-    def test_read_data(self):
-        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-        quik = FileLoaderQUIK()
-        quik.directory = directory
-        file_name1 = "emitents_20052019"
-        file_name2 = "emitents_21052019"
-        main_file_name = "emitents"
-        main_file_path = os.path.join(directory, "".join([main_file_name, ".txt"]))
-
-        data1 = quik.read_data(file_name=file_name1)
-        data2 = quik.read_data(file_name=file_name2)
-
-        # Add new data (data2) to main data(data1)
-        # data2[1:] - ignore first row what is column names
-        data1 = data1.append(data2[1:], ignore_index=True)
-        
-        # Uncomment string below if need to merge 2 files(not update main file)
-        # data1.to_csv(main_file_path, index=False)
-
-        main_data = pd.read_csv(main_file_path)
-        main_data = main_data.append(data1[1:], ignore_index=True)
-        print_info(main_data['Name'].size)
-
-    def test_fileload(self):
-        columns = tuple(['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume'])
-        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-        file_name = "emitents_08052019.txt"
-        quik = FileLoaderQUIK(directory=directory, file_name="emitents_08052019", names=columns)
-        data = quik.read_data()
-
-    def test_wrong_num_columns(self):
-        # Указываем неверное количество колонок(8 вместо 9)
-        # Но ничего непроисходит , потому что колонки вручную прописаны уже в FileLoaderQUIK
-        columns = tuple(['Name', 'Date', 'Code', 'ISIN', 'Min', 'Max', 'Open', 'Close', 'Volume'])
-        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-        file_name = "emitents_08052019.txt"
-        quik = FileLoaderQUIK(directory=directory, file_name="emitents_08052019", names=columns[1:])
-        data = quik.read_data()
-        # Test columns names espetional for QUIK file loader
-        self.assertEqual(columns, tuple(data.columns.tolist()))
 
     def test_change_names(self):
         import re
         pat = re.compile('\[+.+\]')
-        directory = os.path.join(os.path.expanduser('~'), "documents", "projects", "BWS", "tests", "TEMP")
-        file_name = "emitents_08052019"
-        quik = FileLoaderQUIK(directory=directory, file_name=file_name)
-        data  = quik.read_data()
+        data  = pd.read_csv(os.path.join(TEST_DIR, "bws.txt"))
         s = data['Name'][0]
 
         def func(word):
@@ -79,6 +31,8 @@ class TestBWS(unittest.TestCase):
         self.assertEqual(len(func(s)), 8)
 
         data['Name'] = data['Name'].map(func)
+        # debug
+        print_info(data['Name'][0])
 
 
 if __name__ == '__main__':
