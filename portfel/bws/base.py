c93476ce1d371efd9037a1e937dfd2928b5be8fa
Risfio <risfio1982@gmail.com>
2019-05-30 20:52:59 +0300

    bws-main: add db property to BWS main class

--- portfel/bws/base.py
+++ portfel/bws/base.py
@@ -3,7 +3,31 @@
 
 
 class BWSBase:
+    """
+    Main BWS class base.
+    Declaration of this class instance:
+
+    class BWS(BWSBase):
+        db = bws.txt
+
+
+    """
     def __init__(self):
         return  super(BWSBase, self).__init__()
+
+    @property
+    def db(self):
+        return self._db
+
+    @db.setter
+    def db(self, value):
+        """
+        Warning!!! Here must be functional to set ablosute path to database file.
+        :param value: database file name with extension(it's important)
+        """
+        self._db = value
+
+    def _load_data(self, *args, **kwargs):
+        self._data = pd.read_csv(self._db)
 
 
