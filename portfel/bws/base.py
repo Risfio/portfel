5c10291b908d62453e93f574fe390e04ca9cce04
Risfio <risfio1982@gmail.com>
2019-06-04 20:01:09 +0300

    bws main: BWSBase class carcas.

--- portfel/bws/base.py
+++ portfel/bws/base.py
@@ -30,4 +30,26 @@ class BWSBase:
     def _load_data(self, *args, **kwargs):
         self._data = pd.read_csv(self._db)
 
+    def emitents_progression_formula(emitent_data_previous, emitent_data_current):
+        """
+        This is editable function.
+        It gets emitent data by 2 week - previous week and current(more info in functions params declaration).
+        'Name', 'ISIN', 'Date'(not needed), 'Open', 'Min', 'Max', 'Close', 'Volume'
+        """
+        pass
+
+    def _get_emitents_progression(self, data, week_previous=None, week_current=0):
+        pass
+
+    def get_groups(self, groups=3, maxnum=10):
+        """
+        Call stack:
+        load data from main database
+        ...
+
+
+        """
+        data = self._load_data()
+        sorted_data = self._get_emitents_progression((data)
+        # to be continue...
 
